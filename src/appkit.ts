b12655ddc34a20659f3f56bd4bfbc8521d3d0897
inambabu030-design <inambabu030@gmail.com>
2025-12-07 16:07:49 +0600

    Initial React dapp

---
+++ src/appkit.ts
@@ -0,0 +1,56 @@
+// src/appkit.ts
+import { createAppKit } from '@reown/appkit';
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
+import { bsc } from '@reown/appkit/networks';
+
+// 1. Your Reown project ID
+const projectId = 'f89aac819fc8f8516ef9010d98d2fe7b';
+
+// 2. Networks (BSC only)
+export const networks = [bsc];
+
+// 3. Adapter
+const wagmiAdapter = new WagmiAdapter({
+  projectId,
+  networks,
+});
+
+// 4. Metadata
+const metadata = {
+  name: 'MYCASH DApp',
+  description: 'MYCASH rewards dApp on BSC',
+  url: 'https://yourusername.github.io/mycash-dapp-react',
+  icons: ['https://mycash911.github.io/mycash/icon.png'],
+};
+
+// 5. Create AppKit (Wallets ONLY)
+export const appKit = createAppKit({
+  adapters: [wagmiAdapter],
+  projectId,
+  networks,
+  defaultNetwork: bsc,
+  metadata,
+
+
+
+ // 🔹 Put Trust + MetaMask (and Coinbase) at top of wallet list
+  // IDs from Reown "Wallets List" docs
+  featuredWalletIds: [
+    // Trust Wallet
+    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
+    // MetaMask
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
+    
+  ],
+
+
+
+
+
+  // ⭐ Disable Social Logins
+  features: {
+    socials: [],        // remove Google / Discord / GitHub
+    email: false,       // remove Email login
+    analytics: false,   // optional
+  },
+});
