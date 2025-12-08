f27bf7cdc513ce8737a0c05c43b2ba2f53be19a9
mycash <inambabu030@gmail.com>
2025-12-08 10:35:04 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,3 @@
-// src/App.jsx
-import React from "react";
-import RewardsPage from "main.jsx"; // 👈 make sure this path & file name are correct
-
-const App = () => {
-  return (
-    <div className="app">
-      <RewardsPage />
-    </div>
-  );
-};
-
-export default App;
+export default function App() {
+  return null; // React just initializes AppKit
+}
